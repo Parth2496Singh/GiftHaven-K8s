f2d0e7a82ae141574bb0ae2c6aa1f3e6672141f1
Parth2496Singh <parthsinghkushwaha24@gmail.com>
2026-04-26 14:19:07 +0530

    Updated vite.config.js for allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    allowedHosts: ["localhost", "127.0.0.1", "gift-ai"],
     hmr: {
       overlay: false,
     },
